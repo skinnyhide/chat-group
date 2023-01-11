0853a74723b783e80fe851c2be9cf4f07dfb6ae1
jamal <you@example.com>
2023-01-12 01:24:25 +0700

    starter template

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black1: "#0B090C",
+        black2: "#120F13",
+        gray1: "#252329",
+        gray2: "#3C393F",
+        blue: "#2F80ED",
+        "font-white": "#E0E0E0",
+        "font-gray": "#828282",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
